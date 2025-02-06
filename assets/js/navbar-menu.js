d732474e9d5bd101b6c1e742793b0554103a137b
Fellipe Reis <reisfellipe11@gmail.com>
2025-02-06 02:03:49 -0300

    creating menu-burger

---
+++ assets/js/navbar-menu.js
@@ -0,0 +1,40 @@
+class MobileNavbar {
+    constructor(){
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);
+        this.navLinks= document.querySelectorAll(navLinks);
+        this.activeClass = "active";
+        this.handleClick = this.handleClick.bind();
+    }
+
+    animateLinks(){
+        this.navLinks.forEach((link, index)=>{
+            link.style.animation
+            ?(link.style.animation = "")
+            :(link.style.animation = `navLinkFade 0.5s ease fowards ${index / 7 + 0.3}s`)
+        })
+    };
+
+    handleClick(){
+        console.log(this)
+        this.navList.classList.toggle(this.activeClass);
+        this.mobileMenu.classList.toggle(this.activeClass);
+        this.animateLinks();
+    }
+
+    addClickEvent(){
+        this.mobileMenu.addEventListener("click", this.handleClick )};
+
+    iniciar(){
+        if(this.mobileMenu){
+            this.addClickEvent();
+        }
+        return this;
+    }
+}
+
+const mobileNavbar = new MobileNavbar(
+    ".mobile-menu",
+    ".nav-list",
+    ".nav-list li"
+);
